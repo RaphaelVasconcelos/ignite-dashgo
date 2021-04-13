d97774f05d4ebed07c95c51231c930305a66ab99
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-04-13 07:12:07 -0300

    Applying responsive chakra ui feature to header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Raphael Vasconcelos</Text>
-        <Text color="gray.300" fontSize="small">
-          raphael.vasconcelos01@gmail.com
-        </Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Raphael Vasconcelos</Text>
+          <Text color="gray.300" fontSize="small">
+            raphael.vasconcelos01@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Raphael Vasconcelos"
